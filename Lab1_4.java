d4d17af8b0d4485b4eebb81e103c121138270ddf
KatanyooThanoosin <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-01-19 00:57:19 +0700

    Add files via upload

---
+++ Lab1_4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Lab1_4 {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter weight in pounds: ");
+        float w = in.nextFloat();
+        System.out.print("Enter height in inches: ");
+        float h = in.nextFloat();
+        h*=0.0254;
+        w*=0.45359237;
+        System.out.print("BMI is "+(w/(h*h)));
+    }
+}
