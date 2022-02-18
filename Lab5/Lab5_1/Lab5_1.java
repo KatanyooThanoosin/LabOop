0d06462473e4843ee123c627b24308b0fd8804d2
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-02-18 15:36:36 +0700

    Update Lab5_1.java

--- Lab5/Lab5_1/Lab5_1.java
+++ Lab5/Lab5_1/Lab5_1.java
@@ -1,17 +1,28 @@
+import java.util.*;
 public class Lab5_1 {
 
     public static void main(String[] args) {
-
+        Scanner in  = new Scanner(System.in);
         Account account = new Account();
         account.setId(1122);
         account.setBalance(20000);
         account.setAnnualInterestRate(4.5);
 
-        account.withdraw(2500);
-        account.deposit(3000);
-
-        System.out.println("Balance: " + account.getBalance());
-        System.out.println("Monthly Interest: " + account.getMonthlyInterest());
-        System.out.println("Date when this account was created: " + account.getDateCreated());
+        System.out.print("Choose withdraw(1) or deposit(2) : ");
+        int choice = in.nextInt();
+        if(choice == 1) {
+            System.out.print("Enter amount : ");
+            int amount = in.nextInt();
+            account.withdraw(amount);
+            System.out.println(account.toString());
+        }
+        else if(choice == 2) {
+            System.out.print("Enter amount : ");
+            int amount = in.nextInt();
+            account.deposit(amount);
+            System.out.println(account.toString());
+        }
+        else System.out.println("ERROR::Please select 1 or 2");
+        in.close();
     }
 }
