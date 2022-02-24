b2cef3769d067b4b4626c089e2e9f72786b20f0e
ktyjapz <88848316+KatanyooThanoosin@users.noreply.github.com>
2022-02-24 17:17:52 +0700

    Update Triangle.java

--- Lab8/Lab8_1/Triangle.java
+++ Lab8/Lab8_1/Triangle.java
@@ -1,21 +1,20 @@
 public class Triangle extends GeometricObject {
 
     // Attributes
-    private double[] side = new double[3];
+    private double side;
 
     // Constructors
-    public Triangle(String color, boolean filled, double[] side) {
+    public Triangle(String color, boolean filled, double side) {
         super(color, filled);
         this.side = side;
     }
 
     // Methods
     public double getArea() {
-        double s = (this.side[0] + this.side[1] + this.side[2]) / 2;
-        return Math.sqrt(s * (s - this.side[0]) * (s - this.side[1]) * (s - this.side[2]));
+        return (Math.sqrt(3) * this.side * this.side) / 4;
     }
 
     public double getPerimeter() {
-        return this.side[0] + this.side[1] + this.side[2];
+        return this.side * 3;
     }
 }
